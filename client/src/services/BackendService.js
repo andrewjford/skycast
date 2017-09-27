b966524d84a02b6927e8bc8d6eed26b048fa8f49
AJ <ajoeford@gmail.com>
2017-09-26 17:21:03 -0700

    added proxy and changed api fetch

--- client/src/services/BackendService.js
+++ client/src/services/BackendService.js
@@ -1,11 +1,9 @@
 import fetch from 'isomorphic-fetch';
 
-const API_URL = "https://skycast-rails.herokuapp.com";
-
 export function fetchCurrent(address) {
-  return fetch(API_URL + "/current?address=" + address)
+  return fetch("/current?address=" + address)
 }
 
 export function fetchHistory(address, time) {
-  return fetch(API_URL + `/history?address=${address}&time=${time}`)
+  return fetch(`/history?address=${address}&time=${time}`)
 }
